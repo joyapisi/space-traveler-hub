5359df784d85a6ac1834dd9569cfe88e12043c41
Joy Phoebe <joyapisi@gmail.com>
2023-05-16 19:06:36 +0300

    add styling for rockets and missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -27,6 +27,7 @@ const Missions = () => {
             <th>Mission</th>
             <th>Description</th>
             <th>Status</th>
+            <th> </th>
           </tr>
         </thead>
         <tbody>
